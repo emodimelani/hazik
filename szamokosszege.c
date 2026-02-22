e532a512e50962356c653e35b24137cb8aa4556b
emodimelani <emodimelani@mailbox.unideb.hu>
2026-02-22 20:55:57 +0100

    Add files via upload

---
+++ szamokosszege.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 100;
+    int osszeg = n * (n + 1) / 2; //a matematikai képlete
+    printf("Az egész számok összege 1-től 100-ig: %d\n", osszeg);
+ 
+    return 0;
+
+
+}
+
+
