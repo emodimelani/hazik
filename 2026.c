5049ebf89a4541508a4ca252c7dd1bd6741c79c1
emodimelani <emodimelani@mailbox.unideb.hu>
2026-03-12 22:28:26 +0100

    Add files via upload

---
+++ 2026.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int nulla = 'A' - 'A';
+    int ketto = 'C' - 'A';
+    int hat   = 'G' - 'A';
+
+    printf("%d%d%d%d\n", ketto, nulla, ketto, hat);
+
+    return nulla;
+}
